36a8867ebb8f5b233120c82c22c0e314bf311286
Ranvir <uk845816@gmail.com>
2025-10-01 19:29:10 +0530

    frontend connection

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ const postModel = require('./models/postData');
 const userModel=require('./models/userData');
 const jwt=require('jsonwebtoken');
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "capncut-new-update.vercel.app", credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const mongoose=require('mongoose');
 const postData = require('./models/postData'); 
-mongoose.connect("mongodb+srv://uk845816_db_user:Tqebj4wXPwQZe0cf@capncutcluster.zv3mcxb.mongodb.net/?retryWrites=true&w=majority&appName=CapnCutCluster", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect("mongodb+srv://uk845816_db_user:Tqebj4wXPwQZe0cf@capncutcluster.zv3mcxb.mongodb.net/?retryWrites=true&w=majority&appName=CapnCutCluster");
 
 
 // handle replies
